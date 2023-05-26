acae73615117f4efd9f68a77a696c14178fd33ed
Priyank <priyankmalviy@gmail.com>
2023-05-26 14:07:32 +0530

    update

---
+++ README.md
@@ -0,0 +1,2 @@
+# Resume
+It is my resume project which based on python flask will be published on azure using app service.
---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/resume')
+def resume():
+    return render_template('PRIYANK_Resume_EX.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+if __name__ == '__main__':
+    app.run()
